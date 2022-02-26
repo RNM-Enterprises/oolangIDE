cdb6965176836cbd14e85928332079959dfb3e1c
Oliver Tansley <olivertansley@btinternet.com>
2022-02-26 18:33:36 +0000

    clear buttons working

--- src/index.jsx
+++ src/index.jsx
@@ -10,20 +10,23 @@ const wasm = import("../build/hackathon");
 
 wasm.then((m) => {
     const App = () => {
-        const [textFieldValue, setTextFieldValue] = useState("");
 		const [fibResult, setFibResult] = useState();
+        const [code,setCode] = useState("");
 
         const handleUserInput = (e) => {
-            setTextFieldValue(e.target.value);
+            setCode(e.target.value)
         };
 
 		const compute = () => {
             if (fibResult != undefined){
-                setFibResult(fibResult +'\n'+ m.fib(name));
+                setFibResult(fibResult +'\n'+ m.fib(code));
             }else{
-                setFibResult(m.fib(name));
+                setFibResult(m.fib(code));
             }
+            console.log(m.fib(code))
 		}
+
+    
 
         const os = ["o0","o1","o2","o3","o4","o5","o6","o7","o8","o9","o10","o11"];
 
@@ -44,7 +47,7 @@ wasm.then((m) => {
                     
                     <HStack p={4}>
                         <VStack justifyContent={"flex-start"} w={"50%"}>
-                            <HStack>
+                            <HStack justifyContent={"center"}>
                             <Text fontSize="lg">Code</Text>
                             <Spacer/>
                             <IconButton
@@ -52,14 +55,15 @@ wasm.then((m) => {
                                 size={"lg"}
                                 aria-label="Run code"
                                 icon={<CloseIcon color={"white"}/>}
+                                onClick={() => setCode("")}
 								/>
                             </HStack>
                             
-                        <Textarea h="75vh" className="input" resize={"none"} onChange={handleChange} />
+                        <Textarea h="75vh" id="input" resize={"none"} value={code} onChange={handleUserInput} />
                         </VStack>
                         
                         <VStack justifyContent={"flex-start"} w={"50%"}>
-                        <HStack>
+                        <HStack justifyContent={"center"}>
                             <Text fontSize="lg">Output</Text>
                             <Spacer/>
                             <IconButton
@@ -73,8 +77,6 @@ wasm.then((m) => {
                             <Textarea h="75vh" resize={"none"} value={fibResult}/>  
                         </VStack>  
                     </HStack>
-                        
-                    
                     <HStack justifyContent={"space-between"} p={4}>
                         {os.map((o) => <Button bg={"#0077ff"} color={"#FFFFFF"}>{o}</Button>)}
                     </HStack>
